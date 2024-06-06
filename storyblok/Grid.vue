1ad78c82dd564e1c94aae385984e123ebc23b0a4
Theresa H <s2110456010@fhooe.at>
2024-06-06 13:03:19 +0200

    added Project Component

--- storyblok/Grid.vue
+++ storyblok/Grid.vue
@@ -1,14 +1,25 @@
 <template>
+
+<div class="flex-col justify-center my-12">
+  <div>
+    <p v-editable="blok.headline" 
+    class="font-serif font-bold text-3xl leading-snug text-center 
+    underline underline-offset-8 decoration-4 decoration-rose-300">
+    {{ blok.headline }}
+    </p>
+  </div>
+  
   <div
-    v-editable="blok"
-    class="container mx-auto grid md:grid-cols-3 gap-12 my-12 place-items-center"
-  >
+    v-editable="blok" class="container mx-auto grid gap-12 my-12 place-items-center">
     <StoryblokComponent
       v-for="blok in blok.columns"
       :key="blok._uid"
       :blok="blok"
     />
   </div>
+
+</div>
+  
 </template>
 
 <script setup>
---
+++ storyblok/Project.vue
@@ -0,0 +1,41 @@
+<template>
+
+<div class="flex p-12 bg-[#f7f6fd] rounded-[5px]">
+    
+    <div>
+        <p v-editable="blok">
+        {{ blok.title }}
+        </p>
+
+        <div v-html="descriptionContent" class="text-slate-500 mt-4 mb-8"></div>
+
+        <p v-editable="blok" class="">
+        {{ blok.date }}
+        </p>
+
+        <div v-editable="blok" class="space-x-2 my-5" >
+        <StoryblokComponent
+          v-for="blok in blok.button"
+          :key="blok._uid"
+          :blok="blok"
+        />
+      </div>
+
+
+    </div>
+
+    <div v-editable="blok" class="w-full ">
+        <img :src="filename" :alt="alt">
+    </div>
+
+    
+   
+</div>
+
+</template>
+
+<script setup>
+const props = defineProps({blok: Object});
+const descriptionContent = computed(() =>
+  renderRichText(props.blok.description));
+</script>
