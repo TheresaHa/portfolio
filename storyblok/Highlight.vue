3eab5bd0ba6fa47d96f1140b2cb2b9d6031edeea
Theresa H <s2110456010@fhooe.at>
2024-06-06 13:48:33 +0200

    added highlight component

---
+++ storyblok/Highlight.vue
@@ -0,0 +1,15 @@
+<template>
+
+    <div class="container flex w-fit bg-rose-100 rounded-xl space-x-2 px-3">
+        <span v-html="emojiIcon"></span>
+        <p v-editable="blok">{{ blok.label }}</p>
+    </div>
+    
+
+</template>
+
+<script setup>
+const props = defineProps({blok: Object});
+const emojiIcon = computed(() =>
+  renderRichText(props.blok.emoji));
+</script>
