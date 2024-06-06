206f5d46a9476f5074a32364135bcbfbd8ae8e8c
Theresa H <s2110456010@fhooe.at>
2024-06-06 13:02:50 +0200

    Footer adjustments

--- storyblok/Page.vue
+++ storyblok/Page.vue
@@ -1,14 +1,18 @@
 <template>
-  <p class="bg-rose-300">Hier kommt NAV</p>
+  <div class="mb-12">
+    <p class="bg-rose-300">Hier kommt NAV</p>
 
-  <div v-editable="blok" class="">
-    <StoryblokComponent 
-    v-for="blok in blok.body" 
-    :key="blok._uid" 
-    :blok="blok" />
+    <div v-editable="blok">
+      <StoryblokComponent 
+      v-for="blok in blok.body" 
+      :key="blok._uid" 
+      :blok="blok" />
+    </div>
+
+  <p class="text-center text-slate-400 text-sm">© Theresa Haslinger - 2024</p>
+
   </div>
-
-  <p>Hier kommt Footer</p>
+  
 </template>
 
 
