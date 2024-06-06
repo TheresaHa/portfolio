d8ab917033bd9c99ad84857656ba095925379eb3
Theresa H <s2110456010@fhooe.at>
2024-06-06 13:48:16 +0200

    Button CSS changes

--- storyblok/Button.vue
+++ storyblok/Button.vue
@@ -1,10 +1,10 @@
 
 <template>
-      <a v-editable="blok" v-if="blok.is_primary" class="bg-rose-300 hover:bg-rose-400 font-medium py-2 px-4 rounded-xl w-24" 
+      <a v-editable="blok" v-if="blok.is_primary" class="bg-rose-300 hover:bg-rose-400 font-medium py-2 px-4 rounded-xl w-fit" 
       :href="blok.link.cached_url" target="_blank">
       {{ blok.label }}
       </a>
-      <a v-editable="blok" v-else class="border border-slate-500 hover:bg-rose-200 hover:border-slate-400 font-medium py-2 px-4 rounded-xl w-24" 
+      <a v-editable="blok" v-else class="border border-slate-500 hover:bg-rose-200 hover:border-slate-400 font-medium py-2 px-4 rounded-xl w-fit" 
       :href="blok.link.cached_url" target="_blank">
       {{ blok.label }}
     </a>
