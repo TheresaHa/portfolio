3401435bd9e9e9c762657bc206a104eed82c4b74
Theresa H <s2110456010@fhooe.at>
2024-06-06 11:43:32 +0200

    Button Styling

--- storyblok/Button.vue
+++ storyblok/Button.vue
@@ -1,10 +1,15 @@
-<template>
-    
-    <a v-editable="blok" class="" :href="blok.link.cached_url" target="_blank">
-    {{ blok.label }}
-    </a>
 
-  </template>
+<template>
+      <a v-editable="blok" v-if="blok.is_primary" class="bg-rose-300 hover:bg-rose-400 font-medium py-2 px-4 rounded-xl w-24" 
+      :href="blok.link.cached_url" target="_blank">
+      {{ blok.label }}
+      </a>
+      <a v-editable="blok" v-else class="border border-slate-500 hover:bg-rose-200 hover:border-slate-400 font-medium py-2 px-4 rounded-xl w-24" 
+      :href="blok.link.cached_url" target="_blank">
+      {{ blok.label }}
+    </a>
+</template>
+  
   
   <script setup>
   defineProps({ blok: Object })
