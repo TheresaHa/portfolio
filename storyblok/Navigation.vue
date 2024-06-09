8f3908f22401dbc1c58f9ebd678f1c82e8672605
Theresa H <s2110456010@fhooe.at>
2024-06-09 15:36:31 +0200

    navigation optimized

--- storyblok/Navigation.vue
+++ storyblok/Navigation.vue
@@ -1,6 +1,6 @@
 <template>
 
-<nav class="bg-rose-200 fixed w-full sm:flex py-4 px-10 md:px-24 shadow-2xl">
+<nav class="bg-rose-200 fixed w-full sm:flex py-4 px-10 md:px-24 shadow-lg xl:shadow-2xl">
     <a href="/" v-editable="blok" class="w-full"> {{ blok.page_title }} </a>
 
     <div class="w-full flex">
