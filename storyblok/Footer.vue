db53e22aa6033cc8d3a4fb8bc3054b281d0804bb
Theresa H <s2110456010@fhooe.at>
2025-06-01 18:19:10 +0200

    changed Footer

--- storyblok/Footer.vue
+++ storyblok/Footer.vue
@@ -1,7 +1,19 @@
 <template>
+
+  <div class="flex justify-center space-x-4">
+    <div v-editable="blok" class="space-x-2" >
+        <StoryblokComponent
+          v-for="blok in blok.button"
+          :key="blok._uid"
+          :blok="blok"
+        />
+      </div>
+
     <p v-editable="blok" class="text-center text-slate-400 text-sm mb-4">
         {{ blok.message }}
     </p>
+  </div>
+    
 
     <div v-editable="blok" class="flex justify-center space-x-4">
       <StoryblokComponent
